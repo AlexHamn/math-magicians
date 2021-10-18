384e452c1160cc1f6006a55ebc202625d6b1fde0
AlexHamn <agalexandro16@gmail.com>
2021-10-18 15:16:48 -0500

    fix linter errors

--- math-magicians/src/index.js
+++ math-magicians/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App.js';
-import reportWebVitals from './reportWebVitals.js';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>

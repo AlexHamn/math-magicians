a12576f05980f4a3d7cbdb349396aea945e4c8fd
AlexHamn <agalexandro16@gmail.com>
2021-10-19 17:42:52 -0500

    delete boilerplate

--- src/App.js
+++ src/App.js
@@ -1,24 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 import Calculator from './components/Calculator';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          You can&apos;t tell me what to do.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
       <Calculator />
     </div>
   );

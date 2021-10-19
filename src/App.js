d7d5f8ff40f3fb7eef101ba3eb5cbdf4cdac3d5c
AlexHamn <agalexandro16@gmail.com>
2021-10-19 18:31:02 -0500

    use class-based components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
+import React from 'react';
 import './App.css';
 import Calculator from './components/Calculator';
 
-function App() {
-  return (
-    <div className="App">
-      <Calculator />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Calculator />
+      </div>
+    );
+  }
 }
 
 export default App;

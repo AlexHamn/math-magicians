888b161f8faa36814dd0d792ad97ed0bd46abad3
AlexHamn <agalexandro16@gmail.com>
2021-10-19 17:15:53 -0500

    add and style calculator component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import Calculator from './Calculator';
+import Calculator from './components/Calculator';
 
 function App() {
   return (
--- src/Calculator.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Calculator() {
-  return (
-    <div>
-      <p>OwO</p>
-    </div>
-  );
-}
